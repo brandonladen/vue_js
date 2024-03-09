74c178e2705c3a8b2b0380ae4133b55dbc03325a
brandonladen <brandonladen486@gmail.com>
2024-03-09 07:39:42 +0300

    added a counter app

---
+++ counterApp/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {ref} from 'vue';
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <h4>The current count is ...</h4>
+    <h1>{{ count }}</h1>
+    <button @click="count++">+</button>
+    <button @click="count--">-</button>
+  </div>
+</template>
+
+<style scoped>
+div {
+  color: black;
+}
+</style>
